6ba267e3d10331331d680db9c7dfcace9360ae7a
sebastianserrano <juancho_sss@hotmail.com>
2019-05-30 23:20:01 -0400

    RENAMED PIPELINE

--- aws/pipeline/stages/staging/attach-role-policy.sh
+++
@@ -1,4 +0,0 @@
-aws iam attach-role-policy \
-	--role-name PortfolioCodeDeployServiceRole \
-	--policy-arn \
-	arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole
--- aws/ssh/create-ssh-keys.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-KEYNAME=$1
-aws ec2 create-key-pair --key-name ${KEYNAME} --query 'KeyMaterial' --output text > ~/.ssh/Portfolio.pem
