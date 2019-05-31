4d34117feb848273aa4940fb29560ad142ca2af0
sebastianserrano <juancho_sss@hotmail.com>
2019-05-31 18:31:27 -0400

    BASE REACT APP

--- aws-pipeline/pipeline/stages/staging/create-service-role.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-aws iam create-role \
-	--role-name PortfolioCodeDeployServiceRole \
-	--assume-role-policy-document \
-	https://raw.githubusercontent.com/sebastianserrano/portfolio/aws-pipeline/aws/IAM/code-deploy-service-role.json
