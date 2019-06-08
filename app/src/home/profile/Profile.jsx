7cb1505a9775bf3630796d214892b5f1cffa242b
sebastianserrano <juancho_sss@hotmail.com>
2019-06-07 22:12:46 -0400

    PROFILE changed incoming props title text

--- app/src/home/profile/Profile.jsx
+++ app/src/home/profile/Profile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Title from './title/Title';
+import Title from '../components/title/Title';
 import Description from './description/Description';
 
 function Profile() {
@@ -7,7 +7,7 @@ function Profile() {
     <div className="container-fluid section">
       <div className="row justify-content-center">
         <div id="profile">
-          <Title />
+          <Title text="Fullstack Web Developer" />
           <Description text="Avionics Engineer with 3 years of experience in software development
                              ranging from IOS mobile apps to cutting edge websites built with many
                              different capabilities. Proven ability to learn a wide spectrum of essential
--- app/src/home/profile/Profile.test.jsx
+++ app/src/home/profile/Profile.test.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import 'jest-dom/extend-expect';
 import Profile from './Profile';
-
-afterEach(cleanup);
 
 describe('Profile', () => {
   it('Should render gracefully', () => {
