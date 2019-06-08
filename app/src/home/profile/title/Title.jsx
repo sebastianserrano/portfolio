687e939571ba2fd4bfdb9126804367316261af0f
sebastianserrano <juancho_sss@hotmail.com>
2019-06-07 21:30:05 -0400

    PROFILE TITLE restructured for props

--- app/src/home/profile/title/Title.jsx
+++ app/src/home/profile/title/Title.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Title() {
+function Title(props) {
+  const { text } = props;
   return (
-    <h4>
-      Fullstack Web Developer
-    </h4>
+    <h4 className="text-center">{text}</h4>
   );
 }
+
+Title.propTypes = {
+  text: PropTypes.string,
+};
+
+Title.defaultProps = {
+  text: 'Fullstack Web Developer',
+};
 
 export default Title;
