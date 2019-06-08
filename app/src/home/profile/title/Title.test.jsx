1c8c827dace161dd888cb3f9e921f4693087e7c6
sebastianserrano <juancho_sss@hotmail.com>
2019-06-07 20:38:36 -0400

    CLEANED TEST got rid of prop text

--- app/src/home/profile/title/Title.test.jsx
+++ app/src/home/profile/title/Title.test.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import 'jest-dom/extend-expect';
 import Title from './Title';
-
-afterEach(cleanup);
 
 describe('Title', () => {
   it('Should render with text', () => {
     const sampleText = 'Fullstack Web Developer';
-    const { getByText } = render(<Title text={sampleText} />);
+    const { getByText } = render(<Title />);
 
     const title = getByText(sampleText);
     expect(title).toHaveTextContent(sampleText);
