0218536129d48dfb756c60e57b2e9ecda77e12c2
sebastianserrano <juancho_sss@hotmail.com>
2019-06-07 22:13:16 -0400

    SKILLS added section

--- app/src/home/Home.jsx
+++ app/src/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './header/Header';
 import Navbar from './navbar/Navbar';
 import Profile from './profile/Profile';
+import Skills from './skills/Skills';
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Header text="Sebastian Serrano" />
       <Navbar />
       <Profile />
+      <Skills />
     </div>
   );
 }
