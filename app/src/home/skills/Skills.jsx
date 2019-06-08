c59d5e8858596feb3ab4028bbb81c2377e7c9d02
sebastianserrano <juancho_sss@hotmail.com>
2019-06-07 22:14:59 -0400

    SKILLS base structure

---
+++ app/src/home/skills/Skills.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Title from '../components/title/Title';
+
+function Skills() {
+  return (
+    <div className="container-fluid section">
+      <div className="row justify-content-center">
+        <div id="skills">
+          <Title text="Skills" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
