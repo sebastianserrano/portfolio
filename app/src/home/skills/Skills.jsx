487a0d668e61a7572a1b48db9d779a5c70bb6b0b
sebastianserrano <juancho_sss@hotmail.com>
2019-06-08 21:58:00 -0400

    SKILLS added skills based on topics

--- app/src/home/skills/Skills.jsx
+++ app/src/home/skills/Skills.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Title from '../components/title/Title';
+import Skillset from './skillset/Skillset';
 
 function Skills() {
   return (
@@ -7,6 +8,13 @@ function Skills() {
       <div className="row justify-content-center">
         <div id="skills">
           <Title text="Skills" />
+          <Skillset topic="Languages" skills={['Javascript', 'Python']} />
+          <Skillset topic="Databases" skills={['Postgresql', 'MongoDB', 'Firebase']} />
+          <Skillset topic="Frontend" skills={['ReactJS', 'Bootstrap', 'HTML', 'CSS']} />
+          <Skillset topic="Backend" skills={['NodeJS', 'REST API']} />
+          <Skillset topic="Cloud" skills={['Amazon Web Services']} />
+          <Skillset topic="Devops" skills={['AWS Cloudformation', 'AWS CodeDeploy', 'AWS CodePipeline', 'Jenkins']} />
+          <Skillset topic="Tools" skills={['Stripe', 'Twilio', 'ElasticSearch', 'Git/Github', 'Docker', 'Ansible']} />
         </div>
       </div>
     </div>
