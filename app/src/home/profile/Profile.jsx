a54d5c3fd62945eab1a20a0c949c6e1e706d4aa2
sebastianserrano <juancho_sss@hotmail.com>
2019-06-10 11:38:14 -0400

    EXTRA DIV removed redundant div for profile

--- app/src/home/profile/Profile.jsx
+++ app/src/home/profile/Profile.jsx
@@ -5,15 +5,13 @@ import Description from './description/Description';
 function Profile() {
   return (
     <section className="container-fluid section">
-      <div className="row justify-content-center">
-        <div id="profile">
-          <Title text="Fullstack Web Developer" />
-          <Description text="Avionics Engineer with 3 years of experience in software development
-                             ranging from IOS mobile apps to cutting edge websites built with many
-                             different capabilities. Proven ability to learn a wide spectrum of essential
-                             technologies to bring ideas to life at scale"
-          />
-        </div>
+      <div className="row justify-content-center" id="profile">
+        <Title text="Fullstack Web Developer" />
+        <Description text="Avionics Engineer with 3 years of experience in software development
+                           ranging from IOS mobile apps to cutting edge websites built with many
+                           different capabilities. Proven ability to learn a wide spectrum of essential
+                           technologies to bring ideas to life at scale"
+        />
       </div>
     </section>
   );
