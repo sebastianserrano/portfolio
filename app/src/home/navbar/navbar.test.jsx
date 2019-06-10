a6fae855429df6f01006a9d0c23b1c294e5a0845
sebastianserrano <juancho_sss@hotmail.com>
2019-06-10 09:33:24 -0400

    CLEANUP removed afterEach function since there is only one test

--- app/src/home/navbar/navbar.test.jsx
+++ app/src/home/navbar/navbar.test.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import 'jest-dom/extend-expect';
 import Navbar from './Navbar';
-
-afterEach(cleanup);
 
 describe('Navbar', () => {
   it('Should render gracefully', () => {
