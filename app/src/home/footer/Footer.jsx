ec899413b1e0ae18f728e0b82dfe0590e760160b
sebastianserrano <juancho_sss@hotmail.com>
2019-06-18 13:12:42 -0400

    LINKEDIN added linkedin account to footer

--- app/src/home/footer/Footer.jsx
+++ app/src/home/footer/Footer.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import Title from '../components/title/Title';
 import Email from './email/Email';
 import Github from './github/Github';
+import Linkedin from './linkedin/Linkedin';
 
 function Profile() {
   return (
     <footer className="container-fluid section" id="contact">
       <div className="row justify-content-center" id="footer">
-        <div className="col-lg-4 col-md-12 col-sm-12 col-12">
+        <div className="col-lg-2 col-md-12 col-sm-12 col-12">
           <Title text="Contact" />
         </div>
         <div className="col-lg-4 col-md-12 col-sm-12 col-12">
           <Email text="sebastianjuan1994@gmail.com" />
         </div>
-        <div className="col-lg-4 col-md-12 col-sm-12 col-12">
+        <div className="col-lg-3 col-md-12 col-sm-12 col-12">
+          <Linkedin text="Sebastian Serrano" />
+        </div>
+        <div className="col-lg-3 col-md-12 col-sm-12 col-12">
           <Github text="Sebastian Serrano" />
         </div>
       </div>
