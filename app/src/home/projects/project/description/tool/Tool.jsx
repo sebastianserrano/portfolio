fe23b5556025b5c16233c842074d3487e99200f4
sebastianserrano <juancho_sss@hotmail.com>
2019-07-23 16:49:49 -0400

    LAYOUT changed tool component layout for better text alignment

--- app/src/home/projects/project/description/tool/Tool.jsx
+++ app/src/home/projects/project/description/tool/Tool.jsx
@@ -6,9 +6,15 @@ import { faSquare } from '@fortawesome/free-solid-svg-icons';
 function Tool(props) {
   const { text } = props;
   return (
-    <div className="tool-item col-lg-4 col-md-6 col-sm-6 col-12" key={text}>
-      <FontAwesomeIcon icon={faSquare} />
-      {text}
+    <div className="col-lg-4 col-sm-6 col-6">
+      <div className="row">
+        <div className="tool-icon col-lg-1 col-md-1 col-sm-1 col-1" key={text}>
+          <FontAwesomeIcon icon={faSquare} />
+        </div>
+        <div className="tool-text col-lg-9 col-md-5 col-sm-9 col-9">
+          {text}
+        </div>
+      </div>
     </div>
   );
 }
