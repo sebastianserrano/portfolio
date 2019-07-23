e4690a71a3cb292252209349d4f90ea0907f3d2a
sebastianserrano <juancho_sss@hotmail.com>
2019-07-23 16:53:20 -0400

    SYNTAX changed for airbnb style guide compliance

--- app/src/home/projects/project/description/tool/toolsItemsFactory/CreateToolsItems.jsx
+++ app/src/home/projects/project/description/tool/toolsItemsFactory/CreateToolsItems.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import Tool from '../Tool';
 
 function createToolsItems(toolsList) {
-  return toolsList.map((item) => {
-    return <Tool text={item} key={item} />;
-  });
+  return toolsList.map(item => <Tool text={item} key={item} />);
 }
 
 export default createToolsItems;
