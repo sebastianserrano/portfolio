8788f1c307dd02ddc3245ccdce4cc8481f1212b1
sebastianserrano <juancho_sss@hotmail.com>
2019-09-09 17:35:39 -0400

    VALIDATE SCRIPT added missing sleep seconds

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 PORT=3000
+RETRY_SECONDS=5
+
 while ! curl -I localhost:${PORT} > /dev/null 2>&1;do
   echo Failed to pass check at port ${PORT}, retrying in ${RETRY_SECONDS} seconds
   sleep $RETRY_SECONDS
