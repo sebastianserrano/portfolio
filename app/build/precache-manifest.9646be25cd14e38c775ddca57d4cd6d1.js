e3372943ae1c69605e022a049ff426789e5612b7
Sebastian Serrano <sebastian@tactable.io>
2020-11-16 21:38:54 -0500

    build

---
+++ app/build/precache-manifest.9646be25cd14e38c775ddca57d4cd6d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe931cdcc6ef775369823d954abdfa72",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bb0592b1b61b7d33cd1c",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "822b22d29d89df480226",
+    "url": "./static/js/2.3aba6c15.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "./static/js/2.3aba6c15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb0592b1b61b7d33cd1c",
+    "url": "./static/js/main.f5297eda.chunk.js"
+  },
+  {
+    "revision": "e86c2ff2f1eb78554e57",
+    "url": "./static/js/runtime-main.0660989e.js"
+  }
+]);
